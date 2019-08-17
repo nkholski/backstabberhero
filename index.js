d44890d58889fb294cef90c4ab597ce2486071f9
Niklas Berg <com@niklasberg.se>
2019-08-18 01:08:15 +0200

    Moving enemies

--- index.js
+++ index.js
@@ -29,17 +29,18 @@ import {
 */
 
 const level = [
+  [0, 10, 1, 10],
   [1, 10, 6, 8],
-  [0, 10, 1, 10]
+  [0, 5, 9, 7]
   //[0, 5, 3, 5] // Horizontell, 5 bitar, start (x,y) = (3,5) // Horizontell, 5 bitar, start (x,y) = (3,5)
 
   //[1, 5, 3, 2] // Vertikal, 2 bitar
 ];
 
-const [up, right, down, left] = [0, 1, 2, 3];
-const defaultBlocked = { top: false, right: false, down: false, left: false };
+const defaultBlocked = { top: false, right: false, bottom: false, left: false };
 
-const getBlocked = (body, l = 2) => {
+const getBlocked = body => {
+  body.dy += 0.2;
   body.blocked = { ...defaultBlocked };
   const col = body.blocked;
 
@@ -49,7 +50,7 @@ const getBlocked = (body, l = 2) => {
     overlap.left = 16 + item[2] * 16 + 16 * (horizontal ? item[1] : 0) - body.x; // Överlapp på vänster sida
     overlap.right = 16 + body.x - item[2] * 16; // Överlapp på höger sida
     overlap.top = 16 + item[3] * 16 + 16 * (horizontal ? 0 : item[1]) - body.y;
-    overlap.bottom = 16 + body.y - item[3] * 16;
+    overlap.bottom = body.height + body.y - item[3] * 16;
 
     // All sides overlap == collision
     if (
@@ -69,9 +70,16 @@ const getBlocked = (body, l = 2) => {
       });
       col[cdir] = true;
       // Separate bodies
-      body.y =
-        col.top || col.bottom ? 16 * Math.round(sprite.y / 16) : sprite.y;
-      body.x = col.left || col.right ? 16 * Math.round(body.x / 16) : sprite.x;
+      body.y = col.top || col.bottom ? 16 * Math.round(body.y / 16) : body.y;
+      body.x = col.left || col.right ? 16 * Math.round(body.x / 16) : body.x;
+
+      // Stop velocity if colliding
+      body.dx = col.left || col.right ? 0 : body.dx;
+      body.dy = col.top || col.bottom ? 0 : body.dy;
+    }
+
+    if (body.name === "hello") {
+      //  debugger;
     }
   });
 
@@ -86,52 +94,138 @@ let sprite = Sprite({
   y: 16 * 5,
   color: "red", // fill color of the sprite rectangle
   width: 16, // width and height of the sprite rectangle
-  height: 16,
-  blocked: { ...defaultBlocked }
+  height: 32,
+  blocked: { ...defaultBlocked },
+  standing: true
 });
+
+const enemy1 = Sprite({
+  x: 16 * 3, // starting x,y position of the sprite
+  y: 16 * 5,
+  color: "blue", // fill color of the sprite rectangle
+  width: 16, // width and height of the sprite rectangle
+  height: 32,
+  blocked: { ...defaultBlocked },
+  facing: 1, // -1 0 1 == left, none, right
+  dy: 0,
+  walks: true
+});
+
+const enemy2 = Sprite({
+  x: 16 * 9, // starting x,y position of the sprite
+  y: 16 * 5,
+  color: "blue", // fill color of the sprite rectangle
+  width: 16, // width and height of the sprite rectangle
+  height: 32,
+  blocked: { ...defaultBlocked },
+  facing: -1, // -1 0 1 == left, none, right
+  dy: 0,
+  walks: true
+});
+
+const enemies = [enemy1, enemy2];
+
+const checkCliff = body => {
+  const tmpBody = {
+    ...body,
+    x: body.x + (body.facing === 1 ? 24 : -8),
+    y: body.y + 8
+  };
+  getBlocked(tmpBody);
+  return tmpBody.blocked;
+  debugger;
+};
+
+const enemyLogic = enemy => {
+  enemy.dx = enemy.walks ? enemy.facing * 0.5 : 0;
+  // console.log(enemy.blocked.right, enemy.blocked.left);
+  const cliff = !checkCliff(enemy).bottom;
+  if (
+    (enemy.blocked.left && enemy.dx < 0) ||
+    (enemy.blocked.right && enemy.dx > 0) ||
+    (enemy.blocked.bottom && cliff)
+  ) {
+    // debugger;
+    enemy.facing = -enemy.facing;
+  }
+};
 
 let loop = GameLoop({
   // create the main game loop
   update: function() {
     // update the game state
-    console.log(sprite.blocked);
+    if (!sprite.standing) {
+      sprite.standing = true;
+      sprite.y -= 16;
+      sprite.height = 32;
+    }
 
     sprite.dx = 0;
     if (keyPressed("left")) {
-      sprite.dx = -2;
+      sprite.dx = -1;
     }
     if (keyPressed("right")) {
-      sprite.dx = 2;
+      sprite.dx = 1;
     }
-    if (keyPressed("up") && sprite.blocked.bottom) {
-      sprite.dy = -5;
-    }
-    sprite.dy += 0.2;
 
-    if (sprite.dy > 0 && sprite.blocked.bottom) {
-      sprite.dy = 0;
+    if (sprite.blocked.bottom) {
+      if (sprite.dy > 0) {
+        sprite.dy = 0;
+      }
+
+      if (keyPressed("up")) {
+        sprite.dy = -4.5;
+      }
+      if (keyPressed("down") && sprite.standing) {
+        console.log("duck");
+        sprite.standing = false;
+        sprite.y += 16;
+        sprite.height = 16;
+      }
     }
+
     if (sprite.dy < 0 && sprite.blocked.up) {
       sprite.dy = 0;
     }
-
     sprite.update();
-
     getBlocked(sprite);
+
+    enemies.forEach(enemy => {
+      enemyLogic(enemy);
+      enemy.update();
+      getBlocked(enemy);
+    });
   },
   render: function() {
     // draw level
     level.forEach(item => {
+      context.beginPath();
+
+      context.fillStyle = "#662F00";
       context.rect(
         item[2] * 16,
         item[3] * 16,
         item[0] === 0 ? item[1] * 16 : 16,
         item[0] === 1 ? item[1] * 16 : 16
       );
+      context.fill();
+
+      context.beginPath();
+
+      context.fillStyle = "#00B500";
+      context.rect(
+        item[2] * 16,
+        item[3] * 16 - 2,
+        item[0] === 0 ? item[1] * 16 : 16,
+        4
+      );
+      context.fill();
     });
-    context.fill();
     // render the game state
     sprite.render();
+    enemies.forEach(enemy => {
+      enemy.render();
+    });
   }
 });
 
